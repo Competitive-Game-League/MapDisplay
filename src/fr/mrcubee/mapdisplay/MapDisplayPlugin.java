7c72a845770c58d07e72bfc350a09745c5b72782
Eliott Belinguier <eliott.belinguier@epitech.eu>
2020-07-25 18:02:04 +0200

    [add] compatibility with 1.10* version

--- src/fr/mrcubee/mapdisplay/MapDisplayPlugin.java
+++ src/fr/mrcubee/mapdisplay/MapDisplayPlugin.java
@@ -1,6 +1,5 @@
 package fr.mrcubee.mapdisplay;
 
-import org.bukkit.Bukkit;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,6 +7,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import javax.imageio.ImageIO;
 import java.io.IOException;
@@ -33,6 +33,7 @@ public class MapDisplayPlugin extends JavaPlugin implements Listener {
 
     @EventHandler
     public void playerJoinEvent(PlayerJoinEvent event) throws MalformedURLException {
+        final Player player = event.getPlayer();
         final int[] display = {0};
         MapFrame mapFrame = MapFrame.createMapFrame(event.getPlayer().getLocation().add(0, 1.5, 0), BlockFace.NORTH);
 
@@ -40,9 +41,18 @@ public class MapDisplayPlugin extends JavaPlugin implements Listener {
             return;
         this.players.put(event.getPlayer(), mapFrame);
         mapFrame.spawn(event.getPlayer());
-        Bukkit.getScheduler().scheduleAsyncRepeatingTask(this, new Runnable() {
+        try {
+            mapFrame.drawn(event.getPlayer(), ImageIO.read(new URL("https://cravatar.eu/helmavatar/" + event.getPlayer().getUniqueId().toString() + "/600")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        new BukkitRunnable() {
             @Override
             public void run() {
+                if (!player.isOnline()) {
+                    this.cancel();
+                    return;
+                }
                 switch (display[0]) {
                     case 0:
                         try {
@@ -62,7 +72,7 @@ public class MapDisplayPlugin extends JavaPlugin implements Listener {
                 if (display[0] > 1)
                     display[0] = 0;
             }
-        }, 0L, 20L);
+        }.runTaskTimerAsynchronously(this, 0L, 20L);
     }
 
     @EventHandler
